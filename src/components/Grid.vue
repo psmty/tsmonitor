8c29ba80ed3932657eb2a0a2733000230c604538
maks <mojpo4tovik@gmail.com>
2024-10-30 13:32:14 +0000

    fix tesst

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -4,7 +4,7 @@
     @click="exportToExcel">Export</button>
     <button
         class="inline-flex items-center ml-2 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-    @click="test">Test</button>
+    @click="loadSource">Test</button>
     <VGrid ref="grid" readonly :columns="columns" :source="source" :plugins="plugins" hide-attribution :theme="theme" />
 </template>
 <script lang="ts" setup>
