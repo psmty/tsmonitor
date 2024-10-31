367e7a7aa80b8c47a0a846ff61ba14167ec2091d
maks <mojpo4tovik@gmail.com>
2024-10-31 18:24:03 +0000

    feat: extract crawler ui and provide more parers

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,58 +2,44 @@
   <div
     class="flex flex-col grow bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 dark:bg-gray-800"
   >
-    <ImportUrlsButton @saveUrls="saveUrlsToDataBase" class="my-5 mx-5 "/>
-    <Grid :data="data" @load-source="loadSource"/>
+    <ImportUrlsButton @saveUrls="saveUrlsToDataBase" class="my-5 mx-5" />
+    <Grid :data="source" @load-source="loadSource" />
   </div>
 </template>
 
 <script setup lang="ts">
+import Grid from "./Grid.vue";
+import ImportUrlsButton from "./ImportUrlsButton.vue";
+import { computed, onBeforeUnmount, onMounted } from "vue";
+import { useCrawler } from './useCrawler';
 
-import Grid from './Grid.vue';
-import ImportUrlsButton from './ImportUrlsButton.vue';
-import {onBeforeUnmount, onMounted, ref} from 'vue';
-import type {ParsedData} from '../services/versions.types.ts';
+const { siteStatuses, addSites, startCrawler, } = useCrawler();
 
-onMounted(() => {
-    loadSource()
+onMounted(async () => {
+  const response = await fetch("/api/list");
+  const sites = await response.json();
+  startCrawler(sites.map((site: { url: string }) => site.url));
+});
+
+let interval: NodeJS.Timeout | null = null;
+
+const source = computed(() => {
+  return [...siteStatuses.value.values()];
 })
-const data = ref(new Map<string, ParsedData>());
-
-const crawler = async (urls: string[] = []) => {
-    const response = await fetch("/api/crawler", {
-        method: 'POST', body: JSON.stringify(urls)});
-    const sites: ParsedData[] = await response.json();
-    console.log(sites)
-    data.value = new Map(sites.map(v => [v.url, v]))
-}
-
-let interval: NodeJS.Timeout | null = null
-const loadSource = async () => {
-    const response = await fetch("/api/list");
-    const sites: ParsedData[] = await response.json();
-    data.value = new Map(sites.map(v => [v.url, v]))
-    const urls: string[] = Array.from(data.value.keys())
-
-    interval = setInterval(() => crawler(urls), 5 * 60 * 1000)
-    crawler(urls)
-}
 
 const saveUrlsToDataBase = async (urls: string[]) => {
   const response = await fetch("/api/list", {
-    method: 'POST', body: JSON.stringify(urls)
+    method: "POST",
+    body: JSON.stringify(urls),
   });
 
   const newAddedUrls = await response.json();
-  await crawler(newAddedUrls);
-}
+  addSites(newAddedUrls);
+};
 
 onBeforeUnmount(() => {
-    if (interval) {
-        clearInterval(interval)
-    }
-})
+  if (interval) {
+    clearInterval(interval);
+  }
+});
 </script>
-
-<style scoped>
-
-</style>
--- src/components/ImportUrlsButton.vue
+++ src/components/ImportUrlsButton.vue
@@ -1,6 +1,7 @@
 <template>
   <span>
-    <label for="import-urls" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Add Urls</label>
+    <label for="import-urls" data-tooltip-target="tooltip-import-urls" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+    >Import from CSV</label>
     <input type="file" accept=".csv" class="hidden" id="import-urls" @change="parseCsv" />
   </span>
 </template>
