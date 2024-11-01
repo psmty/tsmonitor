b65532fee4d910fdaa94fa39acf5299a5d077a6c
maks <mojpo4tovik@gmail.com>
2024-11-01 18:24:13 +0000

    feat: removed signing form

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,13 +3,16 @@ import { getSession } from "auth-astro/server";
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware(async (context, next) => {
-    if (context.url.pathname.includes("/api") && !context.url.pathname.includes("/auth")) {
-        const loggedin = await getSession(context.request);
-        if (!loggedin) {
-            return new Response("Unauthorized", { status: 401 });
-        }
+    const loggedin = await getSession(context.request);
+    if (!loggedin && !context.url.pathname.includes("/api/auth")) {
+        return context.redirect("/api/auth/signin");
     }
+    // if (context.url.pathname.includes("/api") && !context.url.pathname.includes("/auth")) {
+    //     const loggedin = await getSession(context.request);
+    //     if (!loggedin) {
+    //         return new Response("Unauthorized", { status: 401 });
+    //     }
+    // }
     const response = await next();
-    // console.log("auth response", );
     return response;
 });
