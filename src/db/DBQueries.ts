690955eae754cd35bafa40719f29dcaf07ee8189
Andrey <andrey.pohodnya@giraffesoftware.com>
2024-11-08 09:45:59 +0200

    Fix sql query

--- src/db/DBQueries.ts
+++ src/db/DBQueries.ts
@@ -19,14 +19,17 @@ export async function updateSiteSettings(siteData: SitesData) {
 }
 
 export async function setSites(sitesDataArray: SitesData[]) {
-  const sql = `
+const sql = `
+  WITH inserted AS (
     INSERT INTO sites (url, settings)
     VALUES ${sitesDataArray.map((_, i) => `($${i * 2 + 1}, $${i * 2 + 2}::json)`).join(", ")}
     ON CONFLICT (url)
     DO UPDATE SET settings = EXCLUDED.settings
     RETURNING *
-    ORDER BY url;
-  `;
+  )
+  SELECT * FROM inserted
+  ORDER BY url;
+`;
 
   const values = sitesDataArray.flatMap(siteData => [siteData.url, JSON.stringify(siteData.settings)]);
   return await db.query(sql, values);
