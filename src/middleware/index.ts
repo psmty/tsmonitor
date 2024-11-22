a4971f392360a870ed513f83cf6f00e321133700
maks <mojpo4tovik@gmail.com>
2024-11-22 13:39:12 +0000

    always auth

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -5,6 +5,9 @@ import 'dotenv/config';
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware(async (context, next) => {
+    if (process.env.SKIP_AUTH) {
+        return next();
+    }
     const loggedin = await getSession(context.request);
     if (!loggedin && !context.url.pathname.includes("/api/auth")) {
         return context.redirect("/api/auth/signin");
