3a8d0dcbc9c44936c3aeab08c152c2e9b82818b3
Andrey <andrey.pohodnya@giraffesoftware.com>
2024-11-25 13:41:30 +0200

    Fix boolean editor

--- src/components/grid.columns.ts
+++ src/components/grid.columns.ts
@@ -2,7 +2,7 @@ import type { ColumnDataSchemaModel, ColumnRegular, HyperFunc, VNode } from '@re
 import {CustomFieldsName, Environment} from '../services/consts';
 import type { Ref } from 'vue';
 import {booleanDataSource, environmentDataSource, getResourceDataSource} from './select/defaults.ts';
-import {SelectEditor} from './gridEditors/editors.ts';
+import {BooleanEditor, SelectEditor} from './gridEditors/editors.ts';
 const YES_CLASS = 'bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 border border-green-100 dark:border-green-500';
 const NO_CLASS = 'bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-red-100 dark:border-red-400 dark:bg-gray-700 dark:text-red-400';
 const NO_OPT_CLASS = 'bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-purple-100 dark:bg-gray-700 dark:border-purple-500 dark:text-purple-400';
@@ -72,7 +72,7 @@ export const getGridColumns = ({resources}: {resources: string[]}):ColumnRegular
     size: 250,
     sortable: true,
     readonly: false,
-    editor: SelectEditor,
+    editor: BooleanEditor,
     getOptions: () => booleanDataSource,
     cellTemplate: YES_NO_OPT
   },
