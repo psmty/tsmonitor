43aa96e264b25a8364d47cf9489615a29a558502
Andrey <andrey.pohodnya@giraffesoftware.com>
2024-11-26 17:21:35 +0200

    Update group by select

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,7 +10,7 @@
         >
           Choose columns
         </button>
-        <Select :source="groupByOptions" v-model:value="groupBy" />
+        <Select :source="groupByOptions" v-model:value="groupBy" prefix="Group by"/>
         <input type="text"
                class="h-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                v-model="search"
