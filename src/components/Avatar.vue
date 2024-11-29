f4b9647d36f0c0550f1cd38bc610dc502fc037bd
Andrey <andrey.pohodnya@giraffesoftware.com>
2024-11-29 16:45:40 +0200

    Add avatar

---
+++ src/components/Avatar.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="w-8 h-8 rounded-full bg-blue-600 text-white flex items-center justify-center">
+  {{getNameSymbols}}
+</div>
+</template>
+
+<script setup lang="ts">
+import {computed} from 'vue';
+import {getNameInitials, isInitialsAllowed} from '../services';
+
+const props = defineProps<{
+  name: string
+}>()
+
+const getNameSymbols = computed(() => {
+  return isInitialsAllowed(getNameInitials(props.name))
+})
+</script>
